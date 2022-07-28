a4c71b0252b702d0fca2f871ae7403dec7d2b0fe
hiromirhondalee <106946484+hiromirhondalee@users.noreply.github.com>
2022-07-28 13:11:14 +0900

    API integration added

--- src/app.js
+++ src/app.js
@@ -1,1 +1,66 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
 
+  let dayIndex = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  let day = days[dayIndex];
+
+  let currentDate = date.getDate();
+
+  let monthIndex = date.getMonth();
+  let months = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+  let month = months[monthIndex];
+
+  let year = date.getFullYear();
+
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${day}, ${currentDate} ${month} ${year}, ${hours}:${minutes}`;
+}
+
+function displayInfo(response) {
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.name;
+
+  let dateElement = document.querySelector("#last-updated-date-time");
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
+
+  let descriptionElement = document.querySelector("#weather-description");
+  descriptionElement.innerHTML = response.data.weather[0].description;
+
+  let temperatureElement = document.querySelector("#current-temp");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = response.data.main.humidity;
+
+  let windElement = document.querySelector("#wind");
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+}
+
+let apiKey = "b1710895b469eb434ca65896f4e0d1be";
+let cityName = "Tokyo";
+let apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`;
+
+axios.get(apiURL).then(displayInfo);
